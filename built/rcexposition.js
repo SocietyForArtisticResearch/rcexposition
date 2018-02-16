f6c46860b081309f367c74dc71fbd55fb1acd105
Luc Döbereiner <luc@computer.localdomain>
2018-02-16 12:35:09 +0100

    commonmark

--- built/rcexposition.js
+++ built/rcexposition.js
@@ -13,7 +13,7 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 import * as MarkdownIt from "markdown-it";
-const md = new MarkdownIt();
+const md = new MarkdownIt('commonmark');
 let uniqueID = function () {
     var i = 1;
     return function () {
